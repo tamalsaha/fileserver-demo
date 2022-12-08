73d9c73caa2caafc51bbda3931d00658ce667dfe
Tamal Saha <tamal@appscode.com>
2022-12-08 10:46:52 +0600

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ listing file.
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"io"
@@ -18,6 +19,11 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+
+	hw "go.wandrs.dev/http"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -38,12 +44,30 @@ func main() {
 	r.Options("/*", fileServer.ServeHTTP)
 	r.Get("/*", fileServer.ServeHTTP)
 	r.Post("/*", func(w http.ResponseWriter, r *http.Request) {
-		path, err := FileSave(r)
-		if path == "" {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		err := FileSave(*dir, r)
+
+		status := hw.ErrorToAPIStatus(err)
+		code := int(status.Code)
+		// when writing an error, check to see if the status indicates a retry after period
+		if status.Details != nil && status.Details.RetryAfterSeconds > 0 {
+			delay := strconv.Itoa(int(status.Details.RetryAfterSeconds))
+			w.Header().Set("Retry-After", delay)
+		}
+		if code == http.StatusNoContent {
+			w.WriteHeader(code)
 			return
 		}
-		w.Write([]byte(path))
+
+		data, _ := json.Marshal(status)
+		w.Write(data)
+		//return code
+		//
+		//http.toap
+		//if err != nil {
+		//	http.Error(w, err.Error(), http.StatusInternalServerError)
+		//	return
+		//}
+		//w.Write([]byte("Upload successful!"))
 	})
 
 	// http.Handle("/", fileServer)
@@ -55,18 +79,18 @@ func main() {
 const MaxUploadSize = 100 << 20 // 100 MB
 
 // FileSave fetches the file and saves to disk
-func FileSave(dir string, r *http.Request) (string, error) {
+func FileSave(dir string, r *http.Request) error {
 	// left shift 100 << 20 which results in 32*2^20 = 33554432
 	// x << y, results in x*2^y
 	// 1 MB max memory
 	err := r.ParseMultipartForm(1 << 20)
 	if err != nil {
-		return "", err
+		return err
 	}
 	// Retrieve the file from form data
 	f, h, err := r.FormFile("file")
 	if err != nil {
-		return "", err
+		return err
 	}
 	defer f.Close()
 
@@ -75,33 +99,33 @@ func FileSave(dir string, r *http.Request) (string, error) {
 		//// logger.WithError(err).Error("failed to get the size of the uploaded content")
 		//w.WriteHeader(http.StatusInternalServerError)
 		//writeError(w, err)
-		return "", err
+		return err
 	}
 	if size > MaxUploadSize {
 		// logger.WithField("size", size).Info("file size exceeded")
 		// w.WriteHeader(http.StatusRequestEntityTooLarge)
 		// writeError(w, errors.New("uploaded file size exceeds the limit"))
-		return "", errors.New("uploaded file size exceeds the limit")
+		return apierrors.NewRequestEntityTooLargeError("uploaded file size exceeds the limit")
 	}
 
 	filename := h.Filename
 	if filename == "" {
-		return "", errors.New("missing file name")
+		return errors.New("missing file name")
 	}
 
 	fullPath := filepath.Join(dir, r.URL.Path, filename)
 	_ = os.MkdirAll(filepath.Dir(fullPath), os.ModePerm)
 	file, err := os.OpenFile(fullPath, os.O_WRONLY|os.O_CREATE, os.ModePerm)
 	if err != nil {
-		return "", err
+		return err
 	}
 	defer file.Close()
 	// Copy the file to the destination path
 	_, err = io.Copy(file, f)
 	if err != nil {
-		return "", err
+		return err
 	}
-	return fullPath, nil
+	return nil
 }
 
 func getSize(content io.Seeker) (int64, error) {
